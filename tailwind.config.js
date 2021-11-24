63f1f2e3a3d17362310e1a2ec9369daab30ef89c
chamroeunoum <chamroeunoum@gmail.com>
2021-11-24 10:34:35 +0700

    Update welcome page of SCT

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
+
+    darkMode: false, // or 'media' or 'class'
 
     theme: {
         extend: {
